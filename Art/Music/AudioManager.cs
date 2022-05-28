3010e6b766cda53d88a0534f7aa1161f963e70fc
Aless00san <99504826+Aless00san@users.noreply.github.com>
2022-05-28 10:30:52 +0200

    Added source files

---
+++ Art/Music/AudioManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager instance;
+
+    public AudioSource levelMusic;
+
+    public AudioSource hit, pickup;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        instance = this;
+    }
+
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayGameOver()
+    {
+        levelMusic.Stop();
+
+    }
+
+    public void PlayLevelWin()
+    {
+        levelMusic.Stop();
+    }
+
+    public void PlayHit()
+    {
+        hit.Play();
+    }
+
+    public void PlayPickup()
+    {
+        pickup.Play();
+    }
+
+}
